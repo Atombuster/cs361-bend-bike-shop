050aca89657f577461c5225755d668c57daad0ff
A.K.T <126848536+Atombuster@users.noreply.github.com>
2024-11-14 16:06:25 -0800

    did the changes I think

--- bike.rb
+++ bike.rb
@@ -18,14 +18,23 @@ class Bike
 
   def rent!
     self.rented = true
+    puts "Order confirmed, price owed $#{price}."
   end
 
   def add_cargo(item)
-    self.cargo_contents << item
+    if self.pannier_remaining_capacity == 0
+      self.cargo_contents << item
+    else
+      puts "Can't add cargo bike storage is full"
+    end
+
   end
 
   def remove_cargo(item)
     self.cargo_contents.remove(item)
+    if self.pannier_remaining_capacity == self.pannier_capacity
+      puts "There is no bike's cargo to remove"
+    end
   end
 
   def pannier_capacity
